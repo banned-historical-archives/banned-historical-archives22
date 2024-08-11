35b4581cbccb35d1c2a891a66bd8e97674422754
Your Name <you@example.com>
2024-08-11 12:30:41 +0800

    1

---
+++ 0ae734d9-585e-4253-9083-2c7b41bc2b1a.ts
@@ -0,0 +1,42 @@
+export default {
+  "entity": {
+    "id": "0ae734d9-585e-4253-9083-2c7b41bc2b1a",
+    "name": "林杰同志讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "林杰",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/0ae734d9-585e-4253-9083-2c7b41bc2b1a/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/0ae734d9-585e-4253-9083-2c7b41bc2b1a/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/0ae734d9-585e-4253-9083-2c7b41bc2b1a/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "林杰同志讲话",
+        "authors": [
+          "林杰"
+        ],
+        "page_start": 1,
+        "page_end": 3,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": []
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "0ae734d9-585e-4253-9083-2c7b41bc2b1a",
+  "resource_type": "book",
+  "version": 2
+}
