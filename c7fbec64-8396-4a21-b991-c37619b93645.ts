26b30e7af077072f662bd45a065374ffe859a66f
Your Name <you@example.com>
2024-08-11 12:50:52 +0800

    1

---
+++ c7fbec64-8396-4a21-b991-c37619b93645.ts
@@ -0,0 +1,56 @@
+export default {
+  "entity": {
+    "id": "c7fbec64-8396-4a21-b991-c37619b93645",
+    "name": "周恩来在接见首都大专院校红卫兵革命造反总司令部全体红卫兵大会的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "周恩来",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/c7fbec64-8396-4a21-b991-c37619b93645/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/c7fbec64-8396-4a21-b991-c37619b93645/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/c7fbec64-8396-4a21-b991-c37619b93645/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/c7fbec64-8396-4a21-b991-c37619b93645/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/c7fbec64-8396-4a21-b991-c37619b93645/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/c7fbec64-8396-4a21-b991-c37619b93645/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/c7fbec64-8396-4a21-b991-c37619b93645/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/c7fbec64-8396-4a21-b991-c37619b93645/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/c7fbec64-8396-4a21-b991-c37619b93645/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/c7fbec64-8396-4a21-b991-c37619b93645/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/c7fbec64-8396-4a21-b991-c37619b93645/11.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "周恩来在接见首都大专院校红卫兵革命造反总司令部全体红卫兵大会的讲话",
+        "authors": [
+          "周恩来"
+        ],
+        "page_start": 1,
+        "page_end": 11,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 9,
+            "day": 26
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "c7fbec64-8396-4a21-b991-c37619b93645",
+  "resource_type": "book",
+  "version": 2
+}
