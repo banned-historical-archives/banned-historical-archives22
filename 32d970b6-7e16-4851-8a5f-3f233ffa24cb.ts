5533378f98a3f7be1630f5c291f1cd9e4694258e
Your Name <you@example.com>
2024-08-11 12:37:49 +0800

    1

---
+++ 32d970b6-7e16-4851-8a5f-3f233ffa24cb.ts
@@ -0,0 +1,50 @@
+export default {
+  "entity": {
+    "id": "32d970b6-7e16-4851-8a5f-3f233ffa24cb",
+    "name": "陈伯达同志、王力同志在政协礼堂接见重庆大学“八·一五”战斗队及其他革命师生会上的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "陈伯达 王力",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/32d970b6-7e16-4851-8a5f-3f233ffa24cb/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/32d970b6-7e16-4851-8a5f-3f233ffa24cb/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/32d970b6-7e16-4851-8a5f-3f233ffa24cb/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/32d970b6-7e16-4851-8a5f-3f233ffa24cb/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "陈伯达同志、王力同志在政协礼堂接见重庆大学“八·一五”战斗队及其他革命师生会上的讲话",
+        "authors": [
+          "陈伯达",
+          "王力"
+        ],
+        "page_start": 1,
+        "page_end": 4,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 9,
+            "day": 25
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "32d970b6-7e16-4851-8a5f-3f233ffa24cb",
+  "resource_type": "book",
+  "version": 2
+}
