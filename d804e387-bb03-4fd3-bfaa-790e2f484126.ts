d23bd2b26cc3288fa1f2a5b4f5ebf1a3d0aa6778
Your Name <you@example.com>
2024-09-17 11:50:52 +0800

    1

---
+++ d804e387-bb03-4fd3-bfaa-790e2f484126.ts
@@ -0,0 +1,232 @@
+export default {
+  "entity": {
+    "id": "d804e387-bb03-4fd3-bfaa-790e2f484126",
+    "name": "省市委、省市革委会部分不脱产委员在十二月四日学习班大会上对李一哲反动大字报《关于社会主义的民主与法制》的批判发言",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "中国广州市委批林批孔运动办公室",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/12.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/13.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/14.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/15.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/16.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/17.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/18.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/19.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/20.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/21.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/22.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/23.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives22/main/d804e387-bb03-4fd3-bfaa-790e2f484126/24.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "一株反马克思注意点大毒草——批判反动大字报《关于社会主义的民主与法制》",
+        "authors": [
+          "丘学科"
+        ],
+        "page_start": 2,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "李一哲的《民主与法制》是一株反党反社会主义的大毒草",
+        "authors": [
+          "张永立"
+        ],
+        "page_start": 4,
+        "page_end": 5,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "我们工人要说话",
+        "authors": [
+          "黄嘉炘"
+        ],
+        "page_start": 6,
+        "page_end": 7,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "批判反动大字报《民主与法制》",
+        "authors": [
+          "刘继发"
+        ],
+        "page_start": 8,
+        "page_end": 9,
+        "dates": [
+          {
+            "year": 1874,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "加强无产阶级专政，粉碎资产阶级猖狂进攻——批判反动大字报《民主与法制》",
+        "authors": [
+          "孙亦武"
+        ],
+        "page_start": 10,
+        "page_end": 11,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "无产阶级专政万岁——戳穿李一哲《民主与法制》的外衣",
+        "authors": [
+          "毕波"
+        ],
+        "page_start": 12,
+        "page_end": 13,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "恶毒的用心 卑鄙的手法——批判反动大字报《关于社会主义的民主与法制》",
+        "authors": [
+          "邓惠强"
+        ],
+        "page_start": 13,
+        "page_end": 14,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "《民主与法制》的要害是要搞资产阶级专政",
+        "authors": [
+          "武传斌"
+        ],
+        "page_start": 15,
+        "page_end": 16,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "批判反动大字报《关于社会主义的民主与法制》",
+        "authors": [
+          "薛声钦"
+        ],
+        "page_start": 17,
+        "page_end": 18,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "批判李一哲攻击伟大领袖毛主席和毛泽东思想的反动谬论",
+        "authors": [
+          "刘均益"
+        ],
+        "page_start": 18,
+        "page_end": 19,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "批判李一哲关于工农群众“遭到了剥夺”的谬论",
+        "authors": [
+          "晏寿云"
+        ],
+        "page_start": 20,
+        "page_end": 21,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "蚍蜉撼树谈何易——批判大毒草《民主与法制》",
+        "authors": [
+          "罗泽权"
+        ],
+        "page_start": 21,
+        "page_end": 22,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "愤怒回击李一哲对毛主席、毛泽东思想和毛主席革命路线的恶毒攻击",
+        "authors": [
+          "梁锦棠"
+        ],
+        "page_start": 23,
+        "page_end": 24,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "d804e387-bb03-4fd3-bfaa-790e2f484126",
+  "resource_type": "book",
+  "version": 2
+}
